f0737f720f9ebb95bc1d228a033afed03631f836
Olegs Jeremejevs <olegs@jeremejevs.com>
2014-02-19 18:28:27 +0200

    add new files

--- public/u/conemu-settings.bat
+++ public/u/conemu-settings.bat
@@ -1,5 +1,3 @@
 @ECHO OFF
 
-SET DROPBOX=D:\Dropbox
-
 MKLINK "%APPDATA%\ConEmu.xml" "%DROPBOX%\ConEmu\ConEmu.xml"
--- public/u/sublime-text-settings.bat
+++ public/u/sublime-text-settings.bat
@@ -1,7 +1,5 @@
 @ECHO OFF
 
-SET DROPBOX=D:\Dropbox
-
 MKLINK /D "%APPDATA%\Sublime Text 3\Installed Packages" "%DROPBOX%\Sublime Text 3\Installed Packages"
 MKLINK /D "%APPDATA%\Sublime Text 3\Packages" "%DROPBOX%\Sublime Text 3\Packages"
 MKLINK /D "%APPDATA%\Sublime Text 3\Batch File.sublime-package" "%DROPBOX%\Sublime Text 3\Batch File.sublime-package"
