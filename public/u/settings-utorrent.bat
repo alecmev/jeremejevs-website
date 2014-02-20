b0291be1b0596292bc9bb5ac0f8f88a8bab5691d
Olegs Jeremejevs <olegs@jeremejevs.com>
2014-02-20 02:42:46 +0200

    minor fix to public files

--- public/u/settings-utorrent.bat
+++ public/u/settings-utorrent.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 
-DEL /F /Q "%LOCALAPPDATA%\uTorrent\settings.dat" 2>NUL
+DEL /F /Q "%APPDATA%\uTorrent\settings.dat" 2>NUL
 MKLINK "%APPDATA%\uTorrent\settings.dat" "%DROPBOX%\settings\utorrent\settings.dat"
